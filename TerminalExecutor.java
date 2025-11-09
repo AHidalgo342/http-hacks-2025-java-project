e549990eb4671d899553ccc2ca8a88ea3b3037c8
Alex Hidalgo <cheesewheel64@gmail.com>
2025-11-09 07:45:31 -0800

    Fix video to .gif support.

--- TerminalExecutor.java
+++ TerminalExecutor.java
@@ -104,7 +104,7 @@ public class TerminalExecutor
         }
         else if(isVideo(src) && isGif(fileType))
         {
-            sb.append(" -vf \"split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse\" -loop 0 ");
+            sb.append(" -vf \"fps=10,scale=320:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse\" -loop 0 ");
         }
         else
         {
